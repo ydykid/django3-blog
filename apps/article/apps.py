33a8def9637dd45e750d3b9d276bc526cc529160
link-yang-mt <link.yang@metatalents.com.cn>
2020-05-20 11:07:00 +0800

    article：apps 。。。

--- apps/article/apps.py
+++ apps/article/apps.py
@@ -1,7 +1,9 @@
 from django.apps import AppConfig
+# from django.utils.translation import ugettext_lazy as _
 
 
 class ArticleConfig(AppConfig):
     name = 'article'
     verbose_name = '文章管理'
+    # verbose_name = _('文章管理')
 
